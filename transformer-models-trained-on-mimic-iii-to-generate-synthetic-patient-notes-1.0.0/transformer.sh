8055b5adbccf0bf00b04401eceadd9dc7032591a
Ziming Qi <ziqi@ucsd.edu>
2022-07-25 14:21:46 -0700

    Add files via upload

---
+++ transformer-models-trained-on-mimic-iii-to-generate-synthetic-patient-notes-1.0.0/transformer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd transformer
+
+TEST_FILE=../input.txt
+PROBLEM=mimic_discharge_summaries
+MODEL=transformer
+HPARAMS=transformer_base
+DATA_DIR=./data
+TRAIN_DIR=./output
+USR_DIR=.
+
+BEAM_SIZE=4
+ALPHA=0.6
+DBS=4
+EXTRA_LEN=50
+HPARAMS_OVERRIDE="max_length=10000,max_target_seq_length=512,max_input_seq_length=512"
+
+t2t-decoder \
+    --t2t_usr_dir=$USR_DIR \
+    --data_dir=$DATA_DIR \
+    --problem=$PROBLEM \
+    --model=$MODEL \
+    --hparams_set=$HPARAMS \
+    --hparams=$HPARAMS_OVERRIDE \
+    --output_dir=$TRAIN_DIR \
+    --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA,batch_size=$DBS,extra_length=$EXTRA_LEN" \
+    --decode_from_file=$TEST_FILE \
+    --decode_to_file=../output.txt
